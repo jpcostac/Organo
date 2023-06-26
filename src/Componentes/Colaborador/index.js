8257a0d5c6bbbf804336a2bad0832c9017fd9249
31032019 <jpcostac17@gmail.com>
2023-06-26 13:51:23 -0300

    06_26_2023 Commit

---
+++ src/Componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/jpcostac.png' alt=''/>
+        </div>
+        <div className='rodape'>
+            <h4>João Pedro Costa</h4>
+            <h5>Estudante</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
---
+++ src/Componentes/Usuario/index.js
@@ -0,0 +1,16 @@
+import './Usuario.css'
+
+const Usuario = () => {
+    return(<div>
+            <div className='Caixa'>
+                <h1 className='Iniciais'>JP</h1>
+            </div>
+            <div>
+                <h1 className='NomeUsuario'>Joao Pedro</h1>
+                <h1 className='NomeCargo'>Administrador</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Usuario
